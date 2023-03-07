7f3bb2291541df8f75c42028b80c7ef38f90a2e6
Remy Skelton <124379820+Messi-Sui@users.noreply.github.com>
2023-03-07 15:55:24 +0000

    Finished first part

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Remy Skelton 
+//
+// Created by: Remy Skelton
+// Created on: mar 2023
+// This file contains the JS funtions for index.html
+
+function myButtonClicked () {
+  // This function displays "Hello, World!"
+
+  document.getElementById('hello-world').innerHTML = '<p>Only player with 2 Player of the tornement at the World Cup</p>'
+}
